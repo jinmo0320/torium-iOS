4d5bca27583947e1a0a732ed3e68ee2a379493b9
jinmo0320 <jinmo040320@gmail.com>
2025-12-26 01:19:40 +0900

    initial commit

---
+++ torium-iOS/torium_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  torium_iOSApp.swift
+//  torium-iOS
+//
+//  Created by 최진모 on 12/26/25.
+//
+
+import SwiftUI
+
+@main
+struct torium_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
